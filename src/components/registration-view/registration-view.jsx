07bb223faeb6063f4ba794638145b902302867f1
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2021-11-10 15:14:37 +0100

    added registration-view component

---
+++ src/components/registration-view/registration-view.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+
+export function RegistrationView(props) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [birthdate, setBirthdate] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(username, password, birthdate);
+    props.onRegistration(username);
+    /* Send a request to the server for authentication */
+    /* then call props.onLoggedIn(username) */
+  };
+
+  return (
+    <form>
+      <label>
+        Username:
+        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+      </label>
+      <label>
+        Password:
+        <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+      </label>
+      <label>
+        Birthdate:
+        <input type="date" value={birthdate} onChange={e => setBirthdate(e.target.value)} />
+      </label>
+      <button type="submit" onClick={handleSubmit}>Submit</button>
+    </form>
+  );
+}
+
