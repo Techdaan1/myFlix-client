e218214d8b568888ade7060658ac045cc41c2d92
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2021-11-12 14:40:54 +0100

    Added react-bootstrap componends

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import { Form } from 'react-bootstrap';
+
+import { Form, Button, Card, CardGroup, Container, Col, Row } from 'react-bootstrap';
 
 export function RegistrationView(props) {
   const [username, setUsername] = useState('');
@@ -14,22 +17,54 @@ export function RegistrationView(props) {
   };
 
   return (
-    <form>
-      <h2>Welcome to the registration page of MyFlix</h2>
-      <label>
-        Username:
-        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-      </label>
-      <label>
-        Password:
-        <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
-      </label>
-      <label>
-        Birthdate:
-        <input type="date" value={birthdate} onChange={e => setBirthdate(e.target.value)} />
-      </label>
-      <button type="submit" onClick={handleSubmit}>Submit</button>
-    </form>
+    <Container>
+      <Row>
+        <Col>
+          <CardGroup>
+            <Card>
+              <Card.Body>
+                <Card.Title>Please register to MyFlix</Card.Title>
+                <Form>
+                  <Form.Group>
+                    <Form.Label>Username:</Form.Label>
+                    <Form.Control
+                      type="text"
+                      value={username}
+                      onChange={e => setUsername(e.target.value)}
+                      required
+                      placeholder="Enter username" />
+                  </Form.Group>
+
+                  <Form.Group>
+                    <Form.Label>Password:</Form.Label>
+                    <Form.Control
+                      type="password"
+                      value={password}
+                      onChange={e => setPassword(e.target.value)}
+                      required
+                      placeholder="Enter password"
+                      minLenght="8" />
+                  </Form.Group>
+
+                  <Form.Group>
+                    <Form.Label>Birthdate: </Form.Label>
+                    <Form.Control
+                      type="date"
+                      value={birthdate}
+                      onChange={e => setBirthdate(e.target.value)} />
+                  </Form.Group>
+                  <Button
+                    variant="primary"
+                    type="submit"
+                    onClick={handleSubmit}>Submit
+                  </Button>
+                </Form>
+              </Card.Body>
+            </Card>
+          </CardGroup>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
