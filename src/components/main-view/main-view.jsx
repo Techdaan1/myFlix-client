f218d81e092ccd26d9d96169a5090cc39b0759c0
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2021-11-17 14:25:04 +0100

    imported and added profile view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -9,6 +9,7 @@ import { MovieView } from '../movie-view/movie-view';
 import { DirectorView } from '../director-view/director-view';
 import { GenreView } from '../genre-view/genre-view';
 import { RegistrationView } from '../registration-view/registration-view';
+import { ProfileView } from '../profile-view/profile-view';
 
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -122,6 +123,16 @@ export class MainView extends React.Component {
             if (movies.length === 0) return <div className="main-view" />;
             return <Col md={8}>
               <GenreView genre={movies.find(m => m.Genre.id === match.params.genreId).Genre} onBackClick={() => history.goBack()} />
+            </Col>
+          }} />
+
+          <Route path="/users/:username" render={({ match, history }) => {
+            if (!user) return <Col>
+              <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
+            </Col>
+            if (movies.length === 0) return <div className="main-view" />;
+            return <Col md={8}>
+              <ProfileView history={history} movies={movies} user={user === match.params.username} />
             </Col>
           }} />
         </Row>
