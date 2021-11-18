50ad2223f1652f3162aa9d5f09f255f7b431bac1
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2021-11-18 12:13:29 +0100

    Added some bootstrap styling

--- src/components/genre-view/genre-view.jsx
+++ src/components/genre-view/genre-view.jsx
@@ -1,22 +1,33 @@
 import React from 'react';
 
+import { Container, Row, Col } from 'react-bootstrap';
+
+import './genre-view.scss';
+
 export class GenreView extends React.Component {
   render() {
     const { genre, onBackClick } = this.props;
 
     return (
-      <div className="genre-view">
-        <div className="genre-title">
-          <span className="label">Name: </span>
-          <span className="value">{genre.Name}</span>
-        </div>
-        <div className="genre-description">
-          <span className="label">Description: </span>
-          <span className="value">{genre.Description}</span>
-        </div>
-        <button onClick={() => { onBackClick(null); }}>Back</button>
-      </div>
+      <Container>
+        <Row>
+          <Col>
+            <div className="genre-view">
+              <div className="genre-title">
+                <span className="label">Name: </span>
+                <span className="value">{genre.Name}</span>
+              </div>
 
+              <div className="genre-description">
+                <span className="label">Description: </span>
+                <span className="value">{genre.Description}</span>
+              </div>
+
+              <button onClick={() => { onBackClick(null); }}>Back</button>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
