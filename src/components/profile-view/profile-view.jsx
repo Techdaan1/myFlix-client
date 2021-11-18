fe1fcc91992b67ba60d37dc8607ff8ad81be0e66
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2021-11-18 15:40:00 +0100

    Update profile-view.jsx

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,9 +1,49 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-import { } from 'react-bootstrap';
+import { Container, Row, Col, Card, ListGroup, } from 'react-bootstrap';
 
 import './profile-view.scss';
 
 export class ProfileView extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      Username: null,
+      Password: null,
+      Email: null,
+      Birthday: null,
+      FavoriteMovies: [],
+    };
+  }
+
+  render() {
+    const { Username, Password, Email, Birthday } = this.state;
+    const { movies } = this.props;
+
+    return (
+      <Container>
+        <Row>
+          <Col>
+            <Card>
+              <Card.Body>
+                <Listgroup>
+                  <ListGroup.Item>Username: {Username} </ListGroup.Item>
+                  <ListGroup.Item>Password: *** </ListGroup.Item>
+                  <ListGroup.Item>Email: {Email} </ListGroup.Item>
+                  <ListGroup.Item>Birthday: {Birthday} </ListGroup.Item>
+                </Listgroup>
+              </Card.Body>
+            </Card>
+          </Col>
+
+          <Col>
+
+          </Col>
+
+        </Row>
+      </Container>
+    );
+  }
 }
