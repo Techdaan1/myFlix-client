f1f29a54d47390d591590702cd29cf76f114a40a
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2021-11-18 11:58:55 +0100

    Update genre-view.jsx

---
+++ src/components/genre-view/genre-view.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export class GenreView extends React.Component {
+  render() {
+    const { genre, onBackClick } = this.props;
+
+    return (
+      <div className="genre-view">
+        <div className="genre-title">
+          <span className="label">Name: </span>
+          <span className="value">{genre.Name}</span>
+        </div>
+        <div className="genre-description">
+          <span className="label">Description: </span>
+          <span className="value">{genre.Description}</span>
+        </div>
+        <button onClick={() => { onBackClick(null); }}>Back</button>
+      </div>
+
+    );
+  }
+}
