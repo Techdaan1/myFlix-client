ea3355c5f377f0975c6a3d59f5a03f44a9dc72fc
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2021-11-18 13:17:28 +0100

    import and export

---
+++ src/components/profile-view/profile-view.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+import { } from 'react-bootstrap';
+
+import './profile-view.scss';
+
+export class ProfileView extends React.Component {
+}
