d5fa32e0d37c20bfcbac9958dfee5e41afc87a3f
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2021-11-19 09:32:20 +0100

    Update movie-view.jsx

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -11,23 +11,47 @@ export class MovieView extends React.Component {
     const { movie, onBackClick } = this.props;
 
     return (
-      <Container>
-        <Row className="movie-view">
+
+      <Container className="moviesContainer">
+        <Row>
           <Col>
-            <Card>
-              <Card.Body>
-                <Card.Img className="movie-poster" src={movie.ImagePath} />
-                <Card.Title>{movie.Title}</Card.Title>
-                <Card.Text>{movie.Description}</Card.Text>
+            <div className="movie-view">
+              <div className="movie-poster">
+                <img src={movie.ImagePath} crossOrigin="true" />
+              </div>
+              <div className="movie-title">
+                <span className="title">Title: </span>
+                <span className="value">{movie.Title}</span>
+              </div>
+              <div className="movie-description">
+                <span className="description">Description: </span>
+                <span className="value">{movie.Description}</span>
+              </div>
+              <div className="movie-genre">
+                <span className="genre">Genre: </span>
+                <span className="value">{movie.Genre.Name}</span>
+              </div>
+              <div className="movie-director">
+                <span className="director">Director: </span>
+                <span className="value">{movie.Director.Name}</span>
+              </div>
+
+              <div className="director-button-div">
                 <Link to={`/directors/${movie.Director.Name}`}>
-                  <Button variant="link">{movie.Director.Name}</Button>
+                  <Button className="director-button">Director</Button>
                 </Link>
+              </div>
+
+              <div className="genre-button-div">
                 <Link to={`/genres/${movie.Genre.Name}`}>
-                  <Button variant="link">{movie.Genre.Name}</Button>
+                  <Button className="genre-button">Genre</Button>
                 </Link>
-                <Button onClick={() => { onBackClick(null); }} >Back</Button>
-              </Card.Body>
-            </Card>
+              </div>
+
+              <div className="movie-button-div">
+                <Button className="movie-button" onClick={() => { onBackClick(null); }}>Back</Button>
+              </div>
+            </div>
           </Col>
         </Row>
       </Container>
@@ -35,12 +59,16 @@ export class MovieView extends React.Component {
   }
 }
 
-MovieView.prototype = {
+MovieView.propTypes = {
   movie: PropTypes.shape({
     Title: PropTypes.string.isRequired,
-    Descripton: PropTypes.string,
-    ImagePath: PropTypes.string.isRequired,
-    Genre: PropTypes.array,
-    Director: PropTypes.array
+    Description: PropTypes.string.isRequired,
+    Genre: PropTypes.shape({
+      Name: PropTypes.string.isRequired
+    }),
+    Director: PropTypes.shape({
+      Name: PropTypes.string.isRequired
+    }),
+    ImagePath: PropTypes.string.isRequired
   }).isRequired
 };
