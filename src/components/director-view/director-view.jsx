febe62dd3684651e178a92f16d458ee6def5d4aa
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2021-11-21 12:55:40 +0100

    Update director-view.jsx

--- src/components/director-view/director-view.jsx
+++ src/components/director-view/director-view.jsx
@@ -9,30 +9,26 @@ export class DirectorView extends React.Component {
     const { movie, onBackClick } = this.props;
 
     return (
-      <Container>
-        <Row>
-          <Col>
-            <div className="director-view">
-              <div className="director-name">
-                <span className="name">Name: </span>
-                <span className="value">{movie.Director.Name}</span>
-              </div>
+      <Container className="director-container">
+        <div className="director-view">
+          <div className="director-name">
+            <span className="name">Name: </span>
+            <span className="value">{movie.Director.Name}</span>
+          </div>
 
-              <div className="director-bio">
-                <span className="bio">Bio: </span>
-                <span className="value">{movie.Director.Bio}</span>
-              </div>
+          <div className="director-bio">
+            <span className="bio">Bio: </span>
+            <span className="value">{movie.Director.Bio}</span>
+          </div>
 
-              <div className="director-bio">
-                <span className="bio">Birth: </span>
-                <span className="value">{movie.Director.Birth}</span>
-              </div>
+          <div className="director-bio">
+            <span className="bio">Birth: </span>
+            <span className="value">{movie.Director.Birth}</span>
+          </div>
 
-              <button onClick={() => { onBackClick(null); }}>Back</button>
-            </div>
-          </Col>
-        </Row>
-      </Container>
+          <button onClick={() => { onBackClick(null); }}>Back</button>
+        </div>
+      </Container >
     );
   }
 }
