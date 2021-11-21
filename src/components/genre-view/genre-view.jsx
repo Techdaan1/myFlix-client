10b396d93888ad1e74542ac349e7dc5e361d3587
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2021-11-21 12:55:43 +0100

    Update genre-view.jsx

--- src/components/genre-view/genre-view.jsx
+++ src/components/genre-view/genre-view.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Card, Row, Col } from 'react-bootstrap';
 
 import './genre-view.scss';
 
@@ -9,24 +10,20 @@ export class GenreView extends React.Component {
     const { movie, onBackClick } = this.props;
 
     return (
-      <Container>
-        <Row>
-          <Col>
-            <div className="genre-view">
-              <div className="genre-title">
-                <span className="label">Name: </span>
-                <span className="value">{movie.Genre.Name}</span>
-              </div>
+      <Container className="genre-container">
+        <div className="genre-view">
+          <div className="genre-title">
+            <span className="label">Name: </span>
+            <span className="value">{movie.Genre.Name}</span>
+          </div>
 
-              <div className="genre-description">
-                <span className="label">Description: </span>
-                <span className="value">{movie.Genre.Description}</span>
-              </div>
+          <div className="genre-description">
+            <span className="label">Description: </span>
+            <span className="value">{movie.Genre.Description}</span>
+          </div>
 
-              <button onClick={() => { onBackClick(null); }}>Back</button>
-            </div>
-          </Col>
-        </Row>
+          <button onClick={() => { onBackClick(null); }}>Back</button>
+        </div>
       </Container>
     );
   }
