474fe2437e9e1ff0dc126be591591cebe2dafd58
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2021-11-22 13:24:06 +0100

    Update director-view.jsx

--- src/components/director-view/director-view.jsx
+++ src/components/director-view/director-view.jsx
@@ -1,37 +1,40 @@
-import React from 'react';
+import React from "react";
 
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Button } from "react-bootstrap";
 
-import './director-view.scss';
+import "./director-view.scss";
 
 export class DirectorView extends React.Component {
   render() {
     const { movie, onBackClick } = this.props;
 
     return (
-      <Container>
-        <Row>
-          <Col>
-            <div className="director-view">
-              <div className="director-name">
-                <span className="name">Name: </span>
-                <span className="value">{movie.Director.Name}</span>
-              </div>
+      <Container className="director-container">
+        <div className="director-view">
+          <div className="director-name">
+            <span className="name">Director: </span>
+            <span className="value">{movie.Director.Name}</span>
+          </div>
 
-              <div className="director-bio">
-                <span className="bio">Bio: </span>
-                <span className="value">{movie.Director.Bio}</span>
-              </div>
+          <div className="director-bio">
+            <span className="bio">Bio: </span>
+            <span className="value">{movie.Director.Bio}</span>
+          </div>
 
-              <div className="director-bio">
-                <span className="bio">Birth: </span>
-                <span className="value">{movie.Director.Birth}</span>
-              </div>
+          <div className="director-bio">
+            <span className="bio">Birth: </span>
+            <span className="value">{movie.Director.Birth}</span>
+          </div>
 
-              <button onClick={() => { onBackClick(null); }}>Back</button>
-            </div>
-          </Col>
-        </Row>
+          <Button
+            variant="success"
+            onClick={() => {
+              onBackClick(null);
+            }}
+          >
+            Back
+          </Button>
+        </div>
       </Container>
     );
   }
