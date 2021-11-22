9d5a71859417871290479a7aad3b4c2123fdbfc4
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2021-11-22 13:24:27 +0100

    Create navbar-view.jsx

---
+++ src/components/navbar-view/navbar-view.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import axios from "axios";
+import PropTypes from "prop-types";
+
+import {} from "react-bootstrap";
+
+import "./navbar-view.scss";
+
+export class NavbarView extends React.Component {}
+
+return;
