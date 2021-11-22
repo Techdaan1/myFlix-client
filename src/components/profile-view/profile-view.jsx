a1f09a91eb487c63169737c8becde5f1c458c40d
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2021-11-22 13:24:32 +0100

    Update profile-view.jsx

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import PropTypes from "prop-types";
 
-import { Container, Row, Col, Card, ListGroup, } from 'react-bootstrap';
+import { Container, Row, Col, Card, ListGroup } from 'react-bootstrap';
 
 import './profile-view.scss';
 
@@ -17,32 +18,301 @@ export class ProfileView extends React.Component {
       FavoriteMovies: [],
     };
   }
+
+  componentDidMount() {
+    const accessToken = localStorage.getItem("token");
+    this.getUser(accessToken);
+  }
+
+  onLoggedOut() {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    this.setState({
+      user: null,
+    });
+    window.open("/", "_self");
+  }
+
+  getUser = (token) => {
+    const Username = localStorage.getItem("user");
+    axios
+      .get(`https://myflix-application-2021.herokuapp.com//users/${Username}`, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((response) => {
+        this.setState({
+          Username: response.data.Username,
+          Password: response.data.Password,
+          Email: response.data.Email,
+          Birthday: response.data.Birthday,
+          FavoriteMovies: response.data.FavoriteMovies,
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
+  // Allow user to edit or update profile
+  editUser = (e) => {
+    e.preventDefault();
+    const Username = localStorage.getItem("user");
+    const token = localStorage.getItem("token");
+
+    axios.put(`https://myflix-application-2021.herokuapp.com//users/${Username}`,
+      {
+        Username: this.state.Username,
+        Password: this.state.Password,
+        Email: this.state.Email,
+        Birthday: this.state.Birthday,
+      },
+      {
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    )
+      .then((response) => {
+        this.setState({
+          Username: response.data.Username,
+          Password: response.data.Password,
+          Email: response.data.Email,
+          Birthday: response.data.Birthday,
+        });
+
+        localStorage.setItem("user", this.state.Username);
+        const data = response.data;
+        console.log(data);
+        console.log(this.state.Username);
+        alert("Profile is updated!");
+        window.open(`/users/${Username}`, "_self");
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
+  // Delete a movie from FavoriteMovies list
+  onRemoveFavorite = (e, movie) => {
+    e.preventDefault();
+    const Username = localStorage.getItem("user");
+    const token = localStorage.getItem("token");
+
+    axios.delete(`https://myflix-application-2021.herokuapp.com/users/${Username}/movies/${movie._id}`,
+      {
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    )
+      .then((response) => {
+        console.log(response);
+        alert("Movie is removed!");
+        this.componentDidMount();
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
+  // Deregister
+  onDeleteUser() {
+    const Username = localStorage.getItem("user");
+    const token = localStorage.getItem("token");
+
+    axios
+      .delete(`https://movie-api-by-tammy.herokuapp.com/users/${Username}`, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((response) => {
+        console.log(response);
+        alert("Profile has been deleted.");
+        localStorage.removeItem("user");
+        localStorage.removeItem("token");
+        window.open(`/`, "_self");
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  setUsername(value) {
+    this.setState({
+      Username: value,
+    });
+    this.Username = value;
+  }
+
+  setPassword(value) {
+    this.setState({
+      Password: value,
+    });
+    this.Password = value;
+  }
+
+  setEmail(value) {
+    this.setState({
+      Email: value,
+    });
+    this.Email = value;
+  }
+
+  setBirthday(value) {
+    this.setState({
+      Birthday: value,
+    });
+    this.Birthday = value;
+  }
+
   render() {
-    const { Username, Password, Email, Birthday, FavoriteMovies } = this.state;
+    const { Username, Email, Birthday, FavoriteMovies } = this.state;
     const { movies } = this.props;
 
     return (
-      <Container>
+      <Container className="profile-view">
         <Row>
           <Col>
-            <Card>
+            <Card className="user-profile">
               <Card.Body>
                 <Card.Title>My profile information</Card.Title>
-                <Listgroup>
-                  <ListGroup.Item>Username: {Username} </ListGroup.Item>
-                  <ListGroup.Item>Password: *** </ListGroup.Item>
-                  <ListGroup.Item>Email: {Email} </ListGroup.Item>
-                  <ListGroup.Item>Birthday: {Birthday} </ListGroup.Item>
+                <Listgroup className="profile-info">
+                  <ListGroup.Item className="label">Username: {Username} </ListGroup.Item>
+                  <ListGroup.Item className="label">Password: *** </ListGroup.Item>
+                  <ListGroup.Item clasName="label">Email: {Email} </ListGroup.Item>
+                  <ListGroup.Item className="label">Birthday: {Birthday} </ListGroup.Item>
                 </Listgroup>
               </Card.Body>
             </Card>
           </Col>
-
-          <Col>
-
-          </Col>
-
         </Row>
+        <Row>
+          <Col>
+            <Card className="update-profile">
+              <Card.Body>
+                <Card.Title>Update Profile</Card.Title>
+                <Form
+                  className="update-form"
+                  onSubmit={(e) =>
+                    this.editUser(
+                      e,
+                      this.Username,
+                      this.Password,
+                      this.Email,
+                      this.Birthday
+                    )
+                  }
+                >
+                  <Form.Group>
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control
+                      type="text"
+                      name="Username"
+                      placeholder="New Username"
+                      onChange={(e) => this.setUsername(e.target.value)}
+                      required
+                    />
+                  </Form.Group>
+
+                  <Form.Group>
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                      type="password"
+                      name="Password"
+                      placeholder="New Password"
+                      onChange={(e) => this.setPassword(e.target.value)}
+                      required
+                    />
+                  </Form.Group>
+
+                  <Form.Group>
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control
+                      type="email"
+                      name="Email"
+                      placeholder="Enter Email"
+                      onChange={(e) => this.setEmail(e.target.value)}
+                      required
+                    />
+                  </Form.Group>
+
+                  <Form.Group>
+                    <Form.Label>Birthday</Form.Label>
+                    <Form.Control
+                      type="date"
+                      name="Birthday"
+                      onChange={(e) => this.setBirthday(e.target.value)}
+                    />
+                  </Form.Group>
+                  <div className="bt">
+                    <Button
+                      variant="warning"
+                      type="submit"
+                      onClick={this.editUser}
+                    >
+                      Update User
+                    </Button>
+                    <Button
+                      className="delete-button"
+                      variant="danger"
+                      onClick={() => this.onDeleteUser()}
+                    >
+                      Delete User
+                    </Button>
+                  </div>
+                </Form>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+
+        <Card>
+          <Row style={{ marginTop: "20px" }}>
+            <Col>
+              <h4>{Username} Favorite Movies</h4>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Card.Body>
+                {FavoriteMovies.length === 0 && (
+                  <div className="text-center">No Favorite Movie</div>
+                )}
+                <Row className="favorite-container">
+                  {FavoriteMovies.length > 0 &&
+                    movies.map((movie) => {
+                      if (
+                        movie._id ===
+                        FavoriteMovies.find((fav) => fav === movie._id)
+                      ) {
+                        return (
+                          <Card
+                            className="favorite-movie card-content"
+                            key={movie._id}
+                          >
+                            <Card.Img
+                              className="fav-poster"
+                              variant="top"
+                              src={movie.ImagePath}
+                            />
+                            <Card.Body style={{ backgroundColor: "black" }}>
+                              <Card.Title className="movie_title">
+                                {movie.Title}
+                              </Card.Title>
+
+                              <Button
+                                size="sm"
+                                variant="danger"
+                                value={movie._id}
+                                onClick={(e) => this.onRemoveFavorite(e, movie)}
+                              >
+                                Remove
+                              </Button>
+                            </Card.Body>
+                          </Card>
+                        );
+                      }
+                    })}
+                </Row>
+              </Card.Body>
+            </Col>
+          </Row>
+        </Card>
       </Container>
     );
   }
