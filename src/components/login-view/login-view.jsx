e1d713901f97f5bc16917906c89cf165fded95b7
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2021-11-22 13:04:36 +0100

    Update login-view.jsx

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,48 +1,58 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import propTypes from "prop-types";
+import axios from "axios";
 
-import { Button, Container, Row, Col, Form, Card } from 'react-bootstrap';
+import { Button, Container, Row, Col, Form, Card } from "react-bootstrap";
 
-import './login-view.scss';
+import "./login-view.scss";
 
 export function LoginView(props) {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
     /* Send a request to the server for authentication */
-    axios.post('https://myflix-application-2021.herokuapp.com/login', {
-      Username: username,
-      Password: password
-    })
-      .then(response => {
+    axios
+      .post("https://myflix-application-2021.herokuapp.com/login", {
+        Username: username,
+        Password: password,
+      })
+      .then((response) => {
         const data = response.data;
         props.onLoggedIn(data);
         console.log(username + "successfully logged in!");
       })
-      .catch(e => {
-        console.log('no such user')
+      .catch((e) => {
+        console.log("no such user");
       });
   };
 
   return (
-    <Container>
+    <Container className="login-container">
       <Row>
         <Col></Col>
         <Col>
-          <Card>
+          <Card className="login-card">
             <Form>
               <Form.Group controlId="formUsername">
                 <Form.Label>Username: </Form.Label>
-                <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
+                <Form.Control
+                  type="text"
+                  onChange={(e) => setUsername(e.target.value)}
+                />
               </Form.Group>
 
               <Form.Group controlId="formPassword">
                 <Form.Label>Password: </Form.Label>
-                <Form.Control type="password" onChange={e => setPassword(e.target.value)} />
+                <Form.Control
+                  type="password"
+                  onChange={(e) => setPassword(e.target.value)}
+                />
               </Form.Group>
-              <Button variant="primary" type="submit" onClick={handleSubmit}>Submit</Button>
+              <Button variant="warning" type="submit" onClick={handleSubmit}>
+                Submit
+              </Button>
             </Form>
           </Card>
         </Col>
@@ -51,4 +61,3 @@ export function LoginView(props) {
     </Container>
   );
 }
-
