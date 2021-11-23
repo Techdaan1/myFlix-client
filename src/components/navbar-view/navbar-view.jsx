171e298ae1225f4f5a6cda3e7fdba8b6447934e0
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2021-11-23 10:40:16 +0100

    Revert "Update navbar-view.jsx"
    This reverts commit ec39c9a13332bfac7ebca08e4c818abeb6089677.

--- src/components/navbar-view/navbar-view.jsx
+++ src/components/navbar-view/navbar-view.jsx
@@ -1,64 +1,11 @@
-import React from "react";
-import { Navbar, Container, Nav } from "react-bootstrap";
-import { NavLink } from "react-router-dom";
+import React, { useState } from "react";
+import axios from "axios";
+import PropTypes from "prop-types";
+
+import {} from "react-bootstrap";
 
 import "./navbar-view.scss";
 
-export class NavbarView extends React.Component {
-  constructor() {
-    super();
-    this.state = {};
-  }
+export class NavbarView extends React.Component {}
 
-  onLoggedOut = () => {
-    localStorage.removeItem("token");
-    localStorage.removeItem("user");
-    this.setState({
-      user: null,
-    });
-    window.open("/", "_self");
-  };
-
-  render() {
-    const { user } = this.props;
-    const home = `/`;
-    const profile = `/users/${user}`;
-
-    if (!user) return null;
-
-    return (
-      <Navbar
-        collapseOnSelect
-        expand="lg"
-        variant="light"
-        fixed="top"
-        className="navbar"
-        bg="bg-secondary"
-      >
-        <Container fluid>
-          <Navbar.Brand href="/">MyFlix</Navbar.Brand>
-          <Navbar.Toggle aria-controls="navbarScroll" />
-          <Navbar.Collapse id="navbarScroll">
-            <Nav
-              className="me-auto my-2 my-lg-0"
-              style={{ maxHeight: "100px" }}
-              navbarScroll
-            ></Nav>
-
-            <NavLink to={home} className="nav-link">
-              Home
-            </NavLink>
-
-            <NavLink to={profile} className="nav-link">
-              Profile
-            </NavLink>
-
-            <NavLink to={"/"} onClick={this.onLoggedOut} className="logout">
-              Log Out
-            </NavLink>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    );
-  }
-}
+return;
