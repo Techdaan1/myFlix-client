7644d256ebf2eb69832e8ef971d7029ec3827fd9
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2021-11-23 09:46:49 +0100

    Update director-view.jsx

--- src/components/director-view/director-view.jsx
+++ src/components/director-view/director-view.jsx
@@ -1,35 +1,38 @@
 import React from "react";
 
 import { Container, Row, Col, Button } from "react-bootstrap";
+import { Redirect } from "react-router";
 
 import "./director-view.scss";
 
 export class DirectorView extends React.Component {
   render() {
-    const { movie, onBackClick } = this.props;
+    const director = this.props.movies.find(
+      (m) => m.Director.Name === this.props.match.params.name
+    ).Director;
 
     return (
       <Container className="director-container">
         <div className="director-view">
           <div className="director-name">
             <span className="name">Director: </span>
-            <span className="value">{movie.Director.Name}</span>
+            <span className="value">{director.Name}</span>
           </div>
 
           <div className="director-bio">
             <span className="bio">Bio: </span>
-            <span className="value">{movie.Director.Bio}</span>
+            <span className="value">{director.Bio}</span>
           </div>
 
           <div className="director-bio">
             <span className="bio">Birth: </span>
-            <span className="value">{movie.Director.Birth}</span>
+            <span className="value">{director.Birth}</span>
           </div>
 
           <Button
             variant="success"
             onClick={() => {
-              onBackClick(null);
+              this.props.history.goBack();
             }}
           >
             Back
