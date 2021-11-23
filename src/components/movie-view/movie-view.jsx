d51159246409387da74ec8ca450d1bf797230b1b
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2021-11-23 10:40:13 +0100

    Revert "Update movie-view.jsx"
    This reverts commit 9834854a2fc6aace2ab57b3da824e40348aebfca.

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -6,23 +6,41 @@ import { Container, Row, Col, Button, Card } from "React-bootstrap";
 import { Link } from "react-router-dom";
 
 import "./movie-view.scss";
+import axios from "axios";
 
 export class MovieView extends React.Component {
+  addFavoriteMovie = (e) => {
+    e.preventDefault();
+    const token = localStorage.getItem("token");
+    const username = localStorage.getItem("user");
+    const { movie } = this.props;
+
+    axios
+      .post(
+        `https://myflix-application-2021.herokuapp.com/users/${username}/movies/${movie._id}`,
+        {},
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      )
+      .then((response) => {
+        alert(`Movie added to Favorites`);
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
   render() {
-    /*
-      I have passed the whole movies array to this component so this component can do the find on its own.
-    */
-    console.log(this.props);
-    const movie = this.props.movies.find(
-      (m) => m._id === this.props.match.params.movieId
-    );
+    const { movie, onBackClick } = this.props;
 
     return (
       <Container className="movie-container">
         <Row className="justify-content-md-center">
           <Col>
             <div className="movie-poster">
-              {movie.ImagePath && <img src={movie.ImagePath} />}
+              <img src={movie.ImagePath} />
             </div>
 
             <div className="movie-title">
@@ -32,14 +50,14 @@ export class MovieView extends React.Component {
 
             <div className="movie-director">
               <span className="label">Director: </span>
-              <Link to={`/directors/${movie.Director.Name}`}>
+              <Link to={`/ directors / ${movie.Director.Name} `}>
                 <Button variant="link">{movie.Director.Name}</Button>
               </Link>
             </div>
 
             <div className="movie-genre">
               <span className="label">Genre: </span>
-              <Link to={`/genres/${movie.Genre.Name}`}>
+              <Link to={`/ genres / ${movie.Genre.Name} `}>
                 <Button variant="link">{movie.Genre.Name}</Button>
               </Link>
             </div>
@@ -48,15 +66,25 @@ export class MovieView extends React.Component {
               <span className="label">Description: </span>
               <span className="value">{movie.Description} </span>
             </div>
-
-            <Button
-              className="back-button"
-              onClick={() => {
-                this.props.history.goBack();
-              }}
-            >
-              Back
-            </Button>
+            <Col>
+              <Button
+                className="back-button"
+                onClick={() => {
+                  onBackClick(null);
+                }}
+              >
+                Back
+              </Button>
+            </Col>
+            <Col>
+              <Button
+                variant="success"
+                type="submit"
+                onClick={this.addFavoriteMovie}
+              >
+                Add to Favorites
+              </Button>
+            </Col>
           </Col>
         </Row>
       </Container>
