4418b48b92597bb114a8d5f3f35df17e3ba37bf8
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2021-11-23 10:40:06 +0100

    Revert "Update director-view.jsx"
    This reverts commit 7644d256ebf2eb69832e8ef971d7029ec3827fd9.

--- src/components/director-view/director-view.jsx
+++ src/components/director-view/director-view.jsx
@@ -1,38 +1,35 @@
 import React from "react";
 
 import { Container, Row, Col, Button } from "react-bootstrap";
-import { Redirect } from "react-router";
 
 import "./director-view.scss";
 
 export class DirectorView extends React.Component {
   render() {
-    const director = this.props.movies.find(
-      (m) => m.Director.Name === this.props.match.params.name
-    ).Director;
+    const { movie, onBackClick } = this.props;
 
     return (
       <Container className="director-container">
         <div className="director-view">
           <div className="director-name">
             <span className="name">Director: </span>
-            <span className="value">{director.Name}</span>
+            <span className="value">{movie.Director.Name}</span>
           </div>
 
           <div className="director-bio">
             <span className="bio">Bio: </span>
-            <span className="value">{director.Bio}</span>
+            <span className="value">{movie.Director.Bio}</span>
           </div>
 
           <div className="director-bio">
             <span className="bio">Birth: </span>
-            <span className="value">{director.Birth}</span>
+            <span className="value">{movie.Director.Birth}</span>
           </div>
 
           <Button
             variant="success"
             onClick={() => {
-              this.props.history.goBack();
+              onBackClick(null);
             }}
           >
             Back
