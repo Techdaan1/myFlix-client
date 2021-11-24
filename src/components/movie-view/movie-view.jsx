2cc01bbdfda787c7ea2e7aa1b9e1ac3d3b6eb38f
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2021-11-24 15:34:06 +0100

    Update movie-view.jsx

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import { Container, Row, Col, Button, Card } from "React-bootstrap";
+import { Container, Row, Col, Button, Card, CardGroup } from "React-bootstrap";
 
 import { Link } from "react-router-dom";
 
@@ -25,42 +25,45 @@ export class MovieView extends React.Component {
       <Container className="movie-container">
         <Row className="justify-content-md-center">
           <Col>
-            <div className="movie-poster">
-              {movie.ImagePath && <img src={movie.ImagePath} />}
-            </div>
+            <CardGroup>
+              <Card text-center="true">
+                <div className="movie-poster fluid">
+                  {movie.ImagePath && <img src={movie.ImagePath} />}
+                </div>
 
-            <div className="movie-title">
-              <span className="label">Title: </span>
-              <span className="value">{movie.Title}</span>
-            </div>
+                <div className="movie-title">
+                  <span className="label">Title: </span>
+                  <span className="value">{movie.Title}</span>
+                </div>
 
-            <div className="movie-director">
-              <span className="label">Director: </span>
-              <Link to={`/directors/${movie.Director.Name}`}>
-                <Button variant="link">{movie.Director.Name}</Button>
-              </Link>
-            </div>
+                <div className="movie-director">
+                  <span className="label">Director: </span>
+                  <Link to={`/directors/${movie.Director.Name}`}>
+                    <Button variant="link">{movie.Director.Name}</Button>
+                  </Link>
+                </div>
 
-            <div className="movie-genre">
-              <span className="label">Genre: </span>
-              <Link to={`/genres/${movie.Genre.Name}`}>
-                <Button variant="link">{movie.Genre.Name}</Button>
-              </Link>
-            </div>
+                <div className="movie-genre">
+                  <span className="label">Genre: </span>
+                  <Link to={`/genres/${movie.Genre.Name}`}>
+                    <Button variant="link">{movie.Genre.Name}</Button>
+                  </Link>
+                </div>
 
-            <div className="movie-description">
-              <span className="label">Description: </span>
-              <span className="value">{movie.Description} </span>
-            </div>
-
-            <Button
-              className="back-button"
-              onClick={() => {
-                this.props.history.goBack();
-              }}
-            >
-              Back
-            </Button>
+                <div className="movie-description">
+                  <span className="label">Description: </span>
+                  <span className="value">{movie.Description} </span>
+                </div>
+                <Button
+                  className="back-button mb-2"
+                  onClick={() => {
+                    this.props.history.goBack();
+                  }}
+                >
+                  Back
+                </Button>
+              </Card>
+            </CardGroup>
           </Col>
         </Row>
       </Container>
