971901be2a40a6c89e5b74ff889492d7076351e1
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2021-11-24 15:36:28 +0100

    Update navbar-view.jsx

--- src/components/navbar-view/navbar-view.jsx
+++ src/components/navbar-view/navbar-view.jsx
@@ -1,11 +1,57 @@
-import React, { useState } from "react";
-import axios from "axios";
-import PropTypes from "prop-types";
-
-import {} from "react-bootstrap";
+import React from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
 
 import "./navbar-view.scss";
 
-export class NavbarView extends React.Component {}
+export class NavbarView extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
 
-return;
+  onLoggedOut = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    this.setState({
+      user: null,
+    });
+    window.open("/", "_self");
+  };
+
+  render() {
+    const { user } = this.props;
+    const home = `/`;
+    const profile = `/users/${user}`;
+
+    if (!user) return null;
+
+    return (
+      <Navbar collapseOnSelect expand="lg" variant="light">
+        <Container className="navbar-container">
+          <Navbar.Brand className="app-name">MyFlix</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: "100px" }}
+              navbarScroll
+            ></Nav>
+
+            <NavLink to={home} className="nav-link">
+              Home
+            </NavLink>
+
+            <NavLink to={profile} className="nav-link">
+              My profile
+            </NavLink>
+
+            <NavLink to={"/"} onClick={this.onLoggedOut} className="logout">
+              Log Out
+            </NavLink>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    );
+  }
+}
