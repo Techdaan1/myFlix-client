8ee76afdcde90964110de16f849694d6d9c52998
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2021-11-25 11:46:34 +0100

    Update movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -17,21 +17,27 @@ export class MovieCard extends React.Component {
           <Col>
             <CardGroup>
               <Card text-center="true" style={{ width: "30rem" }}>
-                <Card.Img
-                  className="rounded"
-                  variant="top"
-                  src={movie.ImagePath}
-                  fluid="true"
-                />
                 <Card.Body>
                   <Card.Title>{movie.Title}</Card.Title>
-                  <Card.Text>{movie.Description}</Card.Text>
-                  <Link to={`/movies/${movie._id}`}>
-                    <Button variant="primary">Open</Button>
-                  </Link>
+                  <Card.Img
+                    className="rounded"
+                    variant="top"
+                    src={movie.ImagePath}
+                    fluid="true"
+                  />
+                  <br />
+                  <br />
+                  <div className="text-center">
+                    <Link to={`/movies/${movie._id}`}>
+                      <Button className="btn btn-default" variant="primary">
+                        Movie information
+                      </Button>
+                    </Link>
+                  </div>
                 </Card.Body>
               </Card>
             </CardGroup>
+            <br />
           </Col>
         </Row>
       </Container>
