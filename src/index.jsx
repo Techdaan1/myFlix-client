b8d26bdf95bbdbd7fc2adc7fa91ea391e0ae954c
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2021-11-30 14:47:32 +0100

    imported redux-devtools-extension

--- src/index.jsx
+++ src/index.jsx
@@ -3,13 +3,14 @@ import ReactDOM from "react-dom";
 import Container from "react-bootstrap/Container";
 import { createStore } from "redux";
 import movieApp from "./reducers/reducers";
+import { devToolsEnhancer } from "redux-devtools-extension";
 
 import MainView from "./components/main-view/main-view";
 
 // Import statement to indicate that you need to bundle `./index.scss`
 import "./index.scss";
 
-const store = createStore(movieApp);
+const store = createStore(movieApp, devToolsEnhancer());
 
 // Main component (will eventually use all the others)
 class MyFlixApplication extends React.Component {
