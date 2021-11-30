bb4bb33846237f37865bc9d658f86333a5f38932
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2021-11-30 14:42:58 +0100

    imported React-Redux and created Store

--- src/index.jsx
+++ src/index.jsx
@@ -1,22 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Container from "react-bootstrap/Container";
+import { createStore } from "redux";
+import movieApp from "./reducers/reducers";
+
 import MainView from "./components/main-view/main-view";
 
 // Import statement to indicate that you need to bundle `./index.scss`
 import "./index.scss";
 
+const store = createStore(movieApp);
+
 // Main component (will eventually use all the others)
 class MyFlixApplication extends React.Component {
-  constructor() {
-    super();
-  }
-
   render() {
     return (
-      <Container>
-        <MainView className="bg-black" />
-      </Container>
+      <Provider store={store}>
+        <Container>
+          <MainView className="bg-black" />
+        </Container>
+      </Provider>
     );
   }
 }
